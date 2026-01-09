8137e0c6f139a57cf7813822200c5d91019579a0
Falouhiayoub <falouhiayoub9@gmail.com>
2026-01-09 14:48:07 +0100

    add the components all together

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+        <Header/>
+        <main className="flex-grow pt-16">
+          {children}
+        </main>
+        <Footer/>
       </body>
     </html>
   );
